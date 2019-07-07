b6857e69d1233c5713542ea0ca73dcd38ddcd931
Alexander Ivankov <moonw1nd@yandex-team.ru>
2019-07-08 00:32:40 +0300

    feat: add tests

--- tests/lib/rules/sort-import-specifiers.js
+++ tests/lib/rules/sort-import-specifiers.js
@@ -28,17 +28,79 @@ const ruleTester = new RuleTester();
 ruleTester.run('sort-import-specifiers', rule, {
 
   valid: [
-    "import {a, b, c} from './'",
+    "import {a, b, c} from './';",
+    "import React from 'react';",
+    "import {connect} from 'react-redux';",
+    "import {a1, a2, a10} from './';",
+    "import {A, a1,  a2, a10} from './';",
+    'import * as numbers from "./nums";',
+    "import {A, B,  a2, a10} from './';",
   ],
 
   invalid: [
     {
-      code: "import {b, c, a} from './'",
+      code: 'import {b, a, c} from \'../\';',
       errors: [{
         message: 'Invalid sort import specifiers',
         type: 'ImportDeclaration',
       }],
-      output: "import {a, b, c} from './'",
+      output: 'import {a, b, c} from \'../\';',
+    },
+    {
+      code: 'import t, {b, a, f, c} from \'../\';',
+      errors: [{
+        message: 'Invalid sort import specifiers',
+        type: 'ImportDeclaration',
+      }],
+      output: 'import t, {a, b, c, f} from \'../\';',
+    },
+    {
+      code: 'import t, {b, a10, a1, a2} from \'../\';',
+      errors: [{
+        message: 'Invalid sort import specifiers',
+        type: 'ImportDeclaration',
+      }],
+      output: 'import t, {a1, a2, a10, b} from \'../\';',
+    },
+    {
+      code: 'import t, {b, B, a2, a10, A} from \'../\';',
+      errors: [{
+        message: 'Invalid sort import specifiers',
+        type: 'ImportDeclaration',
+      }],
+      output: 'import t, {A, B, a2, a10, b} from \'../\';',
+    },
+    {
+      code: 'import t, {  b  ,  B  ,  a2  ,  a10  ,  A  } from \'../\';',
+      errors: [{
+        message: 'Invalid sort import specifiers',
+        type: 'ImportDeclaration',
+      }],
+      output: 'import t, {  A  ,  B  ,  a2  ,  a10  ,  b  } from \'../\';',
+    },
+    {
+      code: `
+      import t, {
+        b,
+        B,
+        a2,
+        a10,
+        A,
+      } from '../';
+      `,
+      errors: [{
+        message: 'Invalid sort import specifiers',
+        type: 'ImportDeclaration',
+      }],
+      output: `
+      import t, {
+        A,
+        B,
+        a2,
+        a10,
+        b,
+      } from '../';
+      `,
     },
   ],
 });
